071a30045e159f500194ebea8dedfa89800c8648
secretspy97 <nathan.connor@gosparx.org>
2013-12-04 19:00:59 -0500

    added Victors

--- SparX 2014 Preseason/src/org/gosparx/team1126/Drives.java
+++ SparX 2014 Preseason/src/org/gosparx/team1126/Drives.java
@@ -1,8 +1,6 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package org.gosparx.team1126;
+
+import edu.wpi.first.wpilibj.Victor;
 
 /**
  *
@@ -10,4 +8,35 @@ package org.gosparx.team1126;
  */
 public class Drives {
     
+    private Victor leftDrive1;
+    private Victor leftDrive2;
+    private Victor rightDrive1;
+    private Victor rightDrive2;
+    
+    public Drives(){
+        leftDrive1 = new Victor(IO.PWMLeftDrives1);
+        leftDrive2 = new Victor(IO.PWMLeftDrives2);
+        rightDrive1 = new Victor(IO.PWMRightDrives1);
+        rightDrive1 = new Victor(IO.PWMRightDrives2);
+    }
+    
+    public void setMotorValues(double leftMotor, double rightMotor){
+        
+    }
+    
+    public double getLeftMotorValue(){
+        return 0.0;
+    }
+    
+    public double getRightMotorValue(){
+        return 0.0;
+    }
+    
+    public double getLeftMotorEncoder(){
+        return 0.0;
+    }
+    
+    public double getRightMotorEncoder(){
+        return 0.0;
+    }
 }
